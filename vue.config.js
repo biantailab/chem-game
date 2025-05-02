a0f0bf81d243c9e104679d256218e9733a927ceb
Benzyl-titanium <benzyl.titanium@gmail.com>
2025-05-02 20:48:49 +0800

    first commit

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  publicPath: './',
-  chainWebpack: (config) => {
-    config.plugin('html').tap((args) => {
-      args[0].title = '猜无机化合物游戏'
-      return args
-    })
-  },
-}
